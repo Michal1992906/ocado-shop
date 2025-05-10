8b9b46b516ca67df85c6fb1a89545f6a3f6241f5
Michal Czyzyk <michal.czyzyk1@gmail.com>
2025-05-10 17:44:17 +0200

    Poprawki do strony

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import ProductList from './components/ProductList';
-import Cart from './components/Cart';
-import Checkout from './components/Checkout';
-import { CartProvider } from './context/CartContext';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProductList from "./components/ProductList";
+import Cart from "./components/Cart";
+import Checkout from "./components/Checkout";
+import { CartProvider } from "./context/CartContext";
 
 const App: React.FC = () => {
   return (
     <CartProvider>
-      <Router>
+      <Router basename="/ocado-shop">
         <Routes>
           <Route path="/" element={<ProductList />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/checkout" element={<Checkout />} />
+          <Route path="cart" element={<Cart />} />
+          <Route path="checkout" element={<Checkout />} />
         </Routes>
       </Router>
     </CartProvider>
